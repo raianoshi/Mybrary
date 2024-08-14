139b6934b52a2c81df8fc2a252acae5961d7e8c1
raianoshi <fymedamayaki@gmail.com>
2024-08-14 17:32:45 +0930

    Books Create/Index/New

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
@@ -24,5 +26,6 @@ db.on('error',error=>console.error(error))
 db.once('open',()=>console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', indexRouter)
 
 app.listen(process.env.PORT||3000)
